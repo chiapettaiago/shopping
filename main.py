0b51891055d72d4f40b4be071137cab632401cb9
root <root@vps51526.publiccloud.com.br>
2024-02-07 16:12:01 -0300

    fix

--- main.py
+++ main.py
@@ -81,4 +81,4 @@ def buy(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=9000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
