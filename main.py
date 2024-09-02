c38eb20a37c42cc604a2f674559d40b6c8b4a3c3
Iago Filgueiras Chiapetta <078463@unifeso.lan>
2024-09-02 17:19:31 -0300

    tags

--- main.py
+++ main.py
@@ -944,4 +944,4 @@ if __name__ == '__main__':
         
         # Confirmar as alterações no banco de dados
         db.session.commit()
-    app.run(host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
