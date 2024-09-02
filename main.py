2f346596999c837c9807a7b38643c05724441643
Iago Filgueiras Chiapetta <078463@unifeso.lan>
2024-09-02 17:38:41 -0300

    desativar debug

--- main.py
+++ main.py
@@ -977,4 +977,4 @@ if __name__ == '__main__':
         
         # Confirmar as alterações no banco de dados
         db.session.commit()
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000)
